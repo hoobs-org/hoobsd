d96fe03c533d7bfccc158b555befd2cfff3a3718
Michael Kellsy <mkellsy@gmail.com>
2020-11-01 19:59:02 +0000

    math based accent color

--- src/api/services/themes.ts
+++ src/api/services/themes.ts
@@ -121,7 +121,7 @@ export const DarkTheme: Theme = {
         },
         background: "#141414",
         highlight: "#feb400",
-        accent: "#4b73e3",
+        accent: "#ff6f00",
         dark: "#252525",
         drawer: "#1414149d",
         input: {
@@ -201,7 +201,7 @@ export const LightTheme: Theme = {
         },
         background: "#fcfcfc",
         highlight: "#feb400",
-        accent: "#4b73e3",
+        accent: "#ff6f00",
         dark: "#e7e7e7",
         drawer: "#fcfcfc9d",
         input: {
