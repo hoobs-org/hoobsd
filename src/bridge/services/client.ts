27a967652b7e9176957ce88d3a3d713a43650012
mkellsy <mkellsy@gmail.com>
2021-01-02 21:45:20 -0700

    copyright notices

--- src/bridge/services/client.ts
+++ src/bridge/services/client.ts
@@ -1,6 +1,7 @@
 /**************************************************************************************************
  * hoobsd                                                                                         *
  * Copyright (C) 2020 HOOBS                                                                       *
+ * Copyright (C) 2019 Oznu                                                                        *
  *                                                                                                *
  * This program is free software: you can redistribute it and/or modify                           *
  * it under the terms of the GNU General Public License as published by                           *
